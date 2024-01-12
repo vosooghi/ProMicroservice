39cc011e700a7914a32293bf788912fd269f2d57
Abbas Vosoughi <abbas769@hotmail.com>
2024-01-12 15:05:38 +0330

    NewtonSoft Serilizer implemnted.

---
+++ Ground/Extensions/Serializers/Ground.Extensions.Serializers.NewtonSoft/Ground.Extensions.Serializers.NewtonSoft/Extensions/DependencyInjection/Class1.cs
@@ -0,0 +1,12 @@
+﻿using Ground.Extensions.Serializers.Abstractions;
+using Ground.Extensions.Serializers.NewtonSoft.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ground.Extensions.Serializers.NewtonSoft.Extensions.DependencyInjection
+{
+    public static class NewtonSoftSerializerServiceCollectionExtensions
+    {
+        public static IServiceCollection AddGroundNewtonSoftSerializer(this IServiceCollection services)
+            => services.AddSingleton<IJsonSerializer, NewtonSoftSerializer>();
+    }
+}
--- Ground/Extensions/Serializers/Zamin.Extensions.Serializers.NewtonSoft/Options/AutoMapperOption.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Ground.Extensions.Serializers.NewtonSoft.Options
-{
-    public class AutoMapperOption
-    {
-        public string AssmblyNamesForLoadProfiles { get; set; } = string.Empty;
-    }
-}
