877a770a805b5527ed65460701c9828ba31e066b
Abbas Vosoughi <abbas769@hotmail.com>
2024-01-23 13:18:27 +0330

    Packages updated 2

--- Ground/samples/2.Infra/Data/Ground.Samples.Infra.Data.Sql.Queries/obj/Debug/net7.0/Ground.Samples.Infra.Data.Sql.Queries.AssemblyInfo.cs
+++ Ground/samples/2.Infra/Data/Ground.Samples.Infra.Data.Sql.Queries/obj/Debug/net7.0/Ground.Samples.Infra.Data.Sql.Queries.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ground.Samples.Infra.Data.Sql.Queries")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97d9358742304ecefad56c8da97072d15de379df")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ground.Samples.Infra.Data.Sql.Queries")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ground.Samples.Infra.Data.Sql.Queries")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
