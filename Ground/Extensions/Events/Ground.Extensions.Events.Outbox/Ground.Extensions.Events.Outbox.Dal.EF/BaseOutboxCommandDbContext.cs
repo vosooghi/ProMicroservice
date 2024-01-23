d9cb728ad44b28e40636a0fb98092be6693dda00
Abbas Vosoughi <abbas769@hotmail.com>
2024-01-23 14:20:24 +0330

    Ground.Extensions.Events.OutBox.Dal.EF

---
+++ Ground/Extensions/Events/Ground.Extensions.Events.Outbox/Ground.Extensions.Events.Outbox.Dal.EF/BaseOutboxCommandDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Ground.Extensions.Events.Abstractions;
+using Ground.Extensions.Events.Outbox.Dal.EF.Configs;
+using Ground.Extensions.Events.Outbox.Dal.EF.Interceptors;
+using Ground.Infra.Data.Sql.Commands;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ground.Extensions.Events.Outbox.Dal.EF
+{
+    public abstract class BaseOutboxCommandDbContext : BaseCommandDbContext
+    {
+        public DbSet<OutBoxEventItem> OutBoxEventItems { get; set; }
+
+        public BaseOutboxCommandDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected BaseOutboxCommandDbContext()
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.AddInterceptors(new AddOutBoxEventItemInterceptor());
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfiguration(new OutBoxEventItemConfig());
+        }
+
+
+    }
+}
---
+++ Ground/Extensions/Events/Ground.Extensions.Events.Outbox/Ground.Extensions.Events.Outbox.Dal.EF/Interceptors/AddOutBoxEventItemInterceptor.cs
@@ -0,0 +1,77 @@
+﻿using Ground.Core.Domain.Entities;
+using Ground.Extensions.Events.Abstractions;
+using Ground.Extensions.Serializers.Abstractions;
+using Ground.Extensions.UsersManagement.Abstractions;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace Ground.Extensions.Events.Outbox.Dal.EF.Interceptors
+{
+    public class AddOutBoxEventItemInterceptor : SaveChangesInterceptor
+    {
+        public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+        {
+            AddOutbox(eventData);
+            return base.SavingChanges(eventData, result);
+        }
+
+        public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
+        {
+            AddOutbox(eventData);
+
+            return base.SavingChangesAsync(eventData, result, cancellationToken);
+        }
+
+        private static void AddOutbox(DbContextEventData eventData)
+        {
+            List<dynamic> changedAggregates = eventData.Context.ChangeTracker
+                .Entries<IAggregateRoot>()
+                .Where(x => x.State != EntityState.Detached)
+                .Select(c => c.Entity as dynamic)
+                .Where(c => c.GetEvents() != null && c.GetEvents().Count > 0)
+                .ToList();
+
+            if (changedAggregates is null || !changedAggregates.Any())
+            {
+                return;
+            }
+
+            var userInfoService = eventData.Context.GetService<IUserInfoService>();
+            var serializer = eventData.Context.GetService<IJsonSerializer>();
+            string traceId = string.Empty;
+            string spanId = string.Empty;
+
+            if (Activity.Current != null)
+            {
+                traceId = Activity.Current.TraceId.ToHexString();
+                spanId = Activity.Current.SpanId.ToHexString();
+            }
+
+            foreach (var aggregate in changedAggregates)
+            {
+                var events = aggregate.GetEvents();
+                foreach (object @event in events)
+                {
+                    eventData.Context.Add(new OutBoxEventItem
+                    {
+                        EventId = Guid.NewGuid(),
+                        AccuredByUserId = userInfoService.UserIdOrDefault(),
+                        AccuredOn = DateTime.Now,
+                        AggregateId = aggregate.BusinessId.ToString(),
+                        AggregateName = aggregate.GetType().Name,
+                        AggregateTypeName = aggregate.GetType().FullName ?? aggregate.GetType().Name,
+                        EventName = @event.GetType().Name,
+                        EventTypeName = @event.GetType().FullName ?? @event.GetType().Name,
+                        EventPayload = serializer.Serialize(@event),
+                        TraceId = traceId,
+                        SpanId = spanId,
+                        IsProcessed = false
+                    });
+                }
+            }
+        }
+
+    }
+}
