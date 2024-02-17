708c5415025b1174e1da445577541d51c5dedde4
Abbas Vosoughi <abbas769@hotmail.com>
2024-02-17 21:16:36 +0330

    Steeltoe Service registry/discovery sample implemented.

---
+++ ApiGateway/ServiceRegistry/ServiceRegistry.Client/Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Steeltoe.Common.Discovery;
+using Steeltoe.Discovery;
+
+namespace ServiceRegistry.Client.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public string FirstName { get; set; }
+        private readonly HttpClient _fName;
+        DiscoveryHttpClientHandler _handler;
+        public IndexModel(ILogger<IndexModel> logger, IHttpClientFactory httpClientFactory, IDiscoveryClient client)
+        {
+            _logger = logger;
+            _fName = httpClientFactory.CreateClient("FName");
+            _handler = new DiscoveryHttpClientHandler(client);
+        }
+
+        public async Task OnGet()
+        {
+            var client = new HttpClient(_handler, false);            
+            FirstName = await client.GetStringAsync("http://ServiceRegistry.Src/api/FName");
+        }
+    }
+}
---
+++ ApiGateway/ServiceRegistry/ServiceRegistry.Src/Controllers/FNameController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceRegistry.Src.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FNameController : ControllerBase
+    {
+        private readonly List<string> fNames = new List<string>
+        {
+            "Abbas",
+            "Radmehr",
+            "Alex",
+            "Atarin",
+            "Niv",
+        };
+        private readonly Random _random = new Random();
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var index = _random.Next(0, fNames.Count - 1);
+            return Ok(fNames[index]);
+        }
+    }
+}
