090f5726f9b2974af4941a82f01d07b155e2ae8f
Abbas Vosoughi <abbas769@hotmail.com>
2024-03-01 11:30:26 +0330

    Serilog sink ELK implemented.

--- NewsCMSClient/NewsCMSClient/NewsCMSClient/Program.cs
+++ NewsCMSClient/NewsCMSClient/NewsCMSClient/Program.cs
@@ -1,41 +1,80 @@
-var builder = WebApplication.CreateBuilder(args);
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+using Serilog.Sinks.MSSqlServer;
 
-builder.Services.AddHttpClient("bi", c =>
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day)
+    .CreateBootstrapLogger();
+Log.Information("Starting up");
+
+try
 {
-    c.BaseAddress = new Uri("http://localhost:8000/bi/");
-});//BasicInfo
-builder.Services.AddHttpClient("news", c =>
-{
-    c.BaseAddress = new Uri("http://localhost:8000/news/");
-});//NewsCMS
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+    var builder = WebApplication.CreateBuilder(args);
 
-//Health Check
-builder.Services.AddHealthChecks();
+    builder.Host.UseSerilog((ctx, lc) => lc
+    .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
+    .WriteTo.MSSqlServer(
+        connectionString: "Server=.;Database=LogDb;User Id=sa; Password=P@ssw0rd;encrypt=false",
+        sinkOptions: new MSSqlServerSinkOptions { TableName = "LogEvents", AutoCreateSqlTable = true, AutoCreateSqlDatabase = true })
+    .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://localhost:9200"))
+    {
+        AutoRegisterTemplate = true,
+        AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv6,
+        IndexFormat = "mynewscms-apigateway-index-{0:yyyy.MM}",
+        MinimumLogEventLevel = Serilog.Events.LogEventLevel.Debug,
+    })
+    .Enrich.FromLogContext()
+    .ReadFrom.Configuration(ctx.Configuration));
 
-var app = builder.Build();
+    builder.Services.AddHttpClient("bi", c =>
+    {
+        c.BaseAddress = new Uri("http://localhost:8000/bi/");
+    });//BasicInfo
+    builder.Services.AddHttpClient("news", c =>
+    {
+        c.BaseAddress = new Uri("http://localhost:8000/news/");
+    });//NewsCMS
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    // Add services to the container.
+    builder.Services.AddControllersWithViews();
+
+    //Health Check
+    builder.Services.AddHealthChecks();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    //app.UseHttpsRedirection();
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.MapHealthChecks("health/status");
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+
+    app.Run();
+
 }
-
-//app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapHealthChecks("health/status");
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Unhandled exception");
+}
+finally
+{
+    Log.Information("Shut down complete");
+    Log.CloseAndFlush();
+}
--- NewsCMSClient/NewsCMSClient/NewsCmsApiGateway/Program.cs
+++ NewsCMSClient/NewsCMSClient/NewsCmsApiGateway/Program.cs
@@ -1,22 +1,63 @@
 using NewsCmsApiGateway.Extensions;
 using Steeltoe.Discovery.Client;
+using Serilog;
+using Serilog.Sinks.MSSqlServer;
+using Serilog.Sinks.Elasticsearch;
 
-var builder = WebApplication.CreateBuilder(args);
-//Eureka
-builder.Services.AddDiscoveryClient();
-//Yarp
-builder.Services.AddReverseProxy().LoadFromEureka();
-//.LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+Log.Information("Starting up");
 
-builder.Services.AddHealthChecks();
-
-var app = builder.Build();
-
-app.UseEndpoints(endpoints =>
+try
 {
-    endpoints.MapHealthChecks("health/status");
-});
+    var builder = WebApplication.CreateBuilder(args);
+
+    //Serilog
+    builder.Host.UseSerilog((ctx, lc) => lc
+    .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
+    .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day)
+    .WriteTo.MSSqlServer(
+        connectionString: "Server=.;Database=LogDb;User Id=sa; Password=P@ssw0rd;encrypt=false",
+        sinkOptions: new MSSqlServerSinkOptions { TableName = "LogEvents", AutoCreateSqlTable = true, AutoCreateSqlDatabase = true })
+    .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://localhost:9200"))
+    {
+        AutoRegisterTemplate = true,
+        AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv6,
+        IndexFormat = "mynewscms-client-index-{0:yyyy.MM}",
+        MinimumLogEventLevel = Serilog.Events.LogEventLevel.Debug,
+    })
+    .Enrich.FromLogContext()
+    .ReadFrom.Configuration(ctx.Configuration));
 
 
-app.MapReverseProxy();
-app.Run();
+    //Eureka
+    builder.Services.AddDiscoveryClient();
+    //Yarp
+    builder.Services.AddReverseProxy().LoadFromEureka();
+    //.LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+    builder.Services.AddHealthChecks();
+
+    var app = builder.Build();
+    app.MapReverseProxy();
+    app.UseRouting();
+    app.UseEndpoints(endpoints =>
+    {
+        endpoints.MapHealthChecks("health/live");
+    });
+
+
+    
+    app.Run();
+
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Unhandled exception");
+}
+finally
+{
+    Log.Information("Shut down complete");
+    Log.CloseAndFlush();
+}
