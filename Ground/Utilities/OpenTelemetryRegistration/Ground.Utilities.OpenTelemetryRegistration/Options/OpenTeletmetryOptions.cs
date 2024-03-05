1bc73850d640b827c81f300e0ea9cf855732d62d
Abbas Vosoughi <abbas769@hotmail.com>
2024-03-05 19:28:08 +0330

    Opent Telemtry Utility, not completed.

---
+++ Ground/Utilities/OpenTelemetryRegistration/Ground.Utilities.OpenTelemetryRegistration/Options/OpenTeletmetryOptions.cs
@@ -0,0 +1,15 @@
+﻿using OpenTelemetry;
+
+namespace Ground.Utilities.OpenTelemetryRegistration.Options
+{
+    public class OpenTeletmetryOptions
+    {
+        public string ApplicationName { get; set; }
+        public string ServiceName { get; set; }
+        public string ServiceVersion { get; set; }
+        public string ServiceId { get; set; }
+        public ExportProcessorType ExportProcessorType { get; set; } = ExportProcessorType.Simple;
+        public string OltpEndpoint { get; set; } = "http://localhost:4317";
+        public double SamplingProbability { get; set; } = 0.25;
+    }
+}
